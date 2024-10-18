2840cff35891e52bb1d61e6197293dbf045279da
Arianna Ferrotti <ariannaferrotti@gmail.com>
2024-10-18 14:55:44 +0100

    Applied the lecturer's corrections, on time steps, spatial resolution, courant number, consistency among the definitions, and plots

--- AdvEqKOC.py
+++ AdvEqKOC.py
@@ -2,23 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 u = 1.0
-nx = 80
+nx = 40
 x = np.linspace(0.0, 1.0, nx + 1)
-nt = 40
-dx = 2./nx
+nt = 80
+dx = 1./nx
 dt = 1./nt
 
-# c = u*dt/dx
-# If c > 1, the solution is unstable. If c < 1, the solution is stable and not damping.
+c = u*dt/dx
+print('Courant number =', c)
+# FTBS is stable and damping for 0<=c<=1
 
-# The spacial resolution
-# The time step
-# The initial conditions and arrays for the old and new time steps
-phi = np.where(x%1. < 0.5, np.power(np.sin(2*x*np.pi), 2), 0.)
-phiOld = phi.copy()
+# The initial conditions and arrays for the old arrays for the old and new time steps
 def analytic(x,t):
     return np.where((x-u*t)%1. < 0.5, np.power(np.sin(2*np.pi*(x-u*t)),2), 0.)
-
+phi=np.array(analytic(x,0))
+phiOld=phi.copy()
 
 # Plot the initial conditions
 plt.plot(x, phi, 'k', label = 'initial_conditions')
@@ -32,13 +30,13 @@ plt. pause(1)
 for n in range(nt):
   for j in range(1,nx+1):
 # (avoiding boundary conditions)
-    phi[j] = phiOld[j] - u*dt*(phiOld[j] - phiOld[j - 1])/dx
+    phi[j] = phiOld[j] - c*(phiOld[j] - phiOld[j - 1])
   phi[0]=phi[-1]  
   phiOld = phi.copy()
-  if int(n)%(nt/10)==0: 
+  if n%5==0: 
     plt.cla()
-    plt.plot(x,analytic(x,n*dt),'r',label='analytic')
-    plt.plot(x, phi, 'b', label = 'Time ' + str(n*dt))
+    plt.plot(x,analytic(x,(n+1)*dt),'r',label='analytic')
+    plt.plot(x, phi, 'b', label = 'Time ' + str((n+1)*dt))
     plt.legend(loc = 'best')
     plt.ylabel('$\phi$')
     plt.ylim([0,1])
