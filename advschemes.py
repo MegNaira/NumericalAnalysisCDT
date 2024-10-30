e27abfa634b60c021d00f38f1fce8f554a728545
Arianna Ferrotti <ariannaferrotti@gmail.com>
2024-10-30 17:53:33 +0000

    added variable s to account for a scaling between dx and dt, and the rmse function to evaluate errors

--- advschemes.py
+++ advschemes.py
@@ -11,15 +11,18 @@ import matplotlib.pyplot as plt
 
 class whiz:
     
-    def __init__(self, u=1.0, nx=100, nt=500, x=None, plot=True, squarewave=False):
+    def __init__(self, u=1.0, nx=100, nt=500, s=1.0, x=None, plot=True, squarewave=False):
         #Parameters
         self.u = u
         self.nx = nx
         self.nt = nt 
         
+        #Scaling among dx and dt
+        self.s = s
+        
         #Other derived parameters
-        self.dx = 1./self.nx
-        self.dt = 1./self.nt
+        self.dx = (1./self.nx)/self.s
+        self.dt = (1./self.nt)*self.s
         self.c = self.dt*self.u/self.dx
         
         self.x = np.linspace(0.0, 1.0, self.nx+1)
@@ -29,6 +32,8 @@ class whiz:
         self.phi=self.analytic(0)
         self.phiOld=self.phi.copy()
         self.phiOlder=self.phi.copy()
+        
+        self.figures=plot
         
     #the analytical solution to the advection equation, used for comparison and for setting initial conditions
     def analytic(self,t):
@@ -40,23 +45,24 @@ class whiz:
     
     #function to plot interesting time steps
     def plot_timestep(self,n,scheme):
-        fig, ax = plt.subplots(1,1,figsize=(14,6))
-        ax.cla()
-        ax.plot(self.x,self.analytic((n+1)*self.dt),color='#E98A15',label='Analytical')
-        ax.plot(self.x, self.phi, color='#59114D', label = 'Numerical')
-        ax.legend(loc = 'upper right', fontsize=14)
-        ax.text(0.88,0.78,'t=%.2f'%((n+1)*self.dt), fontsize=16)
-        ax.set_xlabel('x', fontsize=20)
-        ax.tick_params(axis='x', which='major', labelsize=18, width=3, length=7)
-        ax.tick_params(axis='x', which='minor', labelsize=0, width=2, length=5)
-        ax.tick_params(axis='y', which='major', labelsize=18, width=3, length=7)
-        ax.tick_params(axis='y', which='minor', labelsize=0, width=2, length=3)
-        ax.set_ylabel('$\phi$', fontsize=20)
-        ax.set_title(scheme + ', c=%.1f'%(self.c), fontsize=22)
-        ax.set_ylim([-0.1,1.1])
-        fig.savefig(scheme + "_step%i_sqwave"%n + str(self.sw) + ".jpg")
-        plt.show()
-        plt.pause(0.05)
+        if (self.figures):
+            fig, ax = plt.subplots(1,1,figsize=(14,6))
+            ax.cla()
+            ax.plot(self.x,self.analytic((n+1)*self.dt),color='#E98A15',label='Analytical')
+            ax.plot(self.x, self.phi, color='#59114D', label = 'Numerical')
+            ax.legend(loc = 'upper right', fontsize=14)
+            ax.text(0.88,0.78,'t=%.2f'%((n+1)*self.dt), fontsize=16)
+            ax.set_xlabel('x', fontsize=20)
+            ax.tick_params(axis='x', which='major', labelsize=18, width=3, length=7)
+            ax.tick_params(axis='x', which='minor', labelsize=0, width=2, length=5)
+            ax.tick_params(axis='y', which='major', labelsize=18, width=3, length=7)
+            ax.tick_params(axis='y', which='minor', labelsize=0, width=2, length=3)
+            ax.set_ylabel('$\phi$', fontsize=20)
+            ax.set_title(scheme + ', c=%.1f'%(self.c), fontsize=22)
+            ax.set_ylim([-0.1,1.1])
+            fig.savefig(scheme + "_step%i_sqwave"%n + str(self.sw) + ".jpg")
+            plt.show()
+            plt.pause(0.05)
     
     #FTBS - computes the numerical solution and calls the plotting function
     def ftbs(self):
@@ -73,6 +79,8 @@ class whiz:
             if n%y==0:
                 self.plot_timestep(n,scheme)
                 
+        return self.phi
+                
     #FTCS - computes the numerical solution and calls the plotting function
     def ftcs(self):
         scheme='FTCS'
@@ -88,6 +96,8 @@ class whiz:
             y=self.nt/10
             if n%y==0:
                 self.plot_timestep(n,scheme)
+                
+        return self.phi
     
     #CTCS - computes the numerical solution and calls the plotting function
     def ctcs(self):
@@ -105,5 +115,11 @@ class whiz:
             #selecting and plotting 10 timesteps
             y=self.nt/10
             if n%y==0:
-                self.plot_timestep(n,scheme) 
+                self.plot_timestep(n,scheme)
+                
+        return self.phi
+    
+    #Root mean square error, used for convergence analysis
+    def rmse(self,res,exp):
+        return np.sqrt(np.mean((exp-res)**2))
         
