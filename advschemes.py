8a511f76d7cba71bac06eb77f1b551cc541fea1a
Arianna Ferrotti <ariannaferrotti@gmail.com>
2024-10-30 13:44:03 +0000

    created a new class and redefines functions accordingly

--- advschemes.py
+++ advschemes.py
@@ -8,100 +8,98 @@ Created on Fri Oct 25 11:34:36 2024
 import numpy as np
 import matplotlib.pyplot as plt
 
-#the analytical solution to the advection equation, used for comparison and for setting initial conditions
-def analytic(u,x,t):
-    return np.where((x-u*t)%1. < 0.5, np.power(np.sin(2*np.pi*(x-u*t)),2), 0.)
 
-#function to plot interesting time steps
-def plot_timestep(x,dx,dt,n,phi,u,scheme):
-    fig, ax = plt.subplots(1,1,figsize=(14,6))
-    ax.cla()
-    ax.plot(x,analytic(u,x,(n+1)*dt),color='#E98A15',label='Analytical')
-    ax.plot(x, phi, color='#59114D', label = 'Numerical')
-    ax.legend(loc = 'upper right', fontsize=14)
-    ax.text(0.88,0.78,'t=%.2f'%((n+1)*dt), fontsize=16)
-    ax.set_xlabel('x', fontsize=20)
-    ax.tick_params(axis='x', which='major', labelsize=18, width=3, length=7)
-    ax.tick_params(axis='x', which='minor', labelsize=0, width=2, length=5)
-    ax.tick_params(axis='y', which='major', labelsize=18, width=3, length=7)
-    ax.tick_params(axis='y', which='minor', labelsize=0, width=2, length=3)
-    ax.set_ylabel('$\phi$', fontsize=20)
-    ax.set_title(scheme + ', c=%.1f'%(u*dt/dx), fontsize=22)
-    ax.set_ylim([0,1])
-    fig.savefig(scheme + "_step%i.jpg"%n)
-    plt.show()
-    plt.pause(0.05)
-
-#FTBS - sets the initial condition, computes the numerical solution and calls the plotting function
-def ftbs(u,x,nx,nt,dx,dt):
-    scheme='FTBS'
-    #initial condition
-    phiInit = np.array(analytic(u,x,0))
+class whiz:
     
-    phiOld = phiInit.copy()
-    phi=phiOld.copy()
-    c = u*dt/dx
-    
-    for n in range(nt):
-        for j in range(1,nx+1):
-            phi[j] = phiOld[j] - c*(phiOld[j] - phiOld[j-1])
-        phi[0]=phi[-1] 
-        phiOld = phi.copy()
+    def __init__(self, u=1.0, nx=50, nt=250, x=None):
+        #Parameters
+        self.u = u
+        self.nx = nx
+        self.nt = nt 
         
-        #selecting and plotting timesteps
-        y=nt/10
-        if n%y==0:
-            plot_timestep(x,dx,dt,n,phi,u,scheme)
-    return phi       
-
-#FTCS
-def ftcs(u,x,nx,nt,dx,dt):
-    scheme='FTCS'
-    #initial condition
-    phiInit = np.array(analytic(u,x,0))
-    
-    phiOld = phiInit.copy()
-    phi=phiOld.copy()
-    c = u*dt/dx
-    
-    for n in range(nt):
-        for j in range(1,nx):
-            phi[j] = phiOld[j] - c*(phiOld[j+1] - phiOld[j-1])/2
-        phi[0]=phiOld[0]-c*(phiOld[1]-phiOld[-2])/2    
-        phi[-1]=phi[0] 
-        phiOld = phi.copy()
+        #Other derived parameters
+        self.dx = 1./self.nx
+        self.dt = 1./self.nt
+        self.c = self.dt*self.u/self.dx
         
-        #selecting and plotting timesteps
-        y=nt/10
-        if n%y==0:
-            plot_timestep(x,dx,dt,n,phi,u,scheme)
-    return phi  
-
-
-#CTCS
-def ctcs(u,x,nx,nt,dx,dt):
-    scheme='CTCS'
-    #initial condition
-    phiInit = np.array(analytic(u,x,0))
-    phiInit1 = np.array(analytic(u,x,dt))
-    
-    phiOlder = phiInit.copy()
-    phiOld = phiInit1.copy()
-    phi=phiOlder.copy()
-    c = u*dt/dx
-    
-    for n in range(2,nt):
-        for j in range(1,nx):
-            phi[j] = phiOlder[j] - c*(phiOld[j+1] - phiOld[j-1])
-        phi[0]=phiOlder[0]-c*(phiOld[1]-phiOld[-2])   
-        phi[-1]=phi[0] 
-        phiOlder = phiOld.copy()
-        phiOld = phi.copy()
+        self.x = np.linspace(0.0, 1.0, self.nx+1)
         
-        #selecting and plotting timesteps
-        y=nt/10
-        if n%y==0:
-            plot_timestep(x,dx,dt,n,phi,u,scheme)
-    return phi  
+        
+        self.phi=self.analytic(0)
+        self.phiOld=self.phi.copy()
+        self.phiOlder=self.phi.copy()
+        
+    #the analytical solution to the advection equation, used for comparison and for setting initial conditions
+    def analytic(self,t):
+        return np.where((self.x-self.u*t)%1. < 0.5, np.power(np.sin(2*np.pi*(self.x-self.u*t)),2), 0.)
+    
+    
+    #function to plot interesting time steps
+    def plot_timestep(self,n,scheme):
+        fig, ax = plt.subplots(1,1,figsize=(14,6))
+        ax.cla()
+        ax.plot(self.x,self.analytic((n+1)*self.dt),color='#E98A15',label='Analytical')
+        ax.plot(self.x, self.phi, color='#59114D', label = 'Numerical')
+        ax.legend(loc = 'upper right', fontsize=14)
+        ax.text(0.88,0.78,'t=%.2f'%((n+1)*self.dt), fontsize=16)
+        ax.set_xlabel('x', fontsize=20)
+        ax.tick_params(axis='x', which='major', labelsize=18, width=3, length=7)
+        ax.tick_params(axis='x', which='minor', labelsize=0, width=2, length=5)
+        ax.tick_params(axis='y', which='major', labelsize=18, width=3, length=7)
+        ax.tick_params(axis='y', which='minor', labelsize=0, width=2, length=3)
+        ax.set_ylabel('$\phi$', fontsize=20)
+        ax.set_title(scheme + ', c=%.1f'%(self.c), fontsize=22)
+        ax.set_ylim([0,1])
+        fig.savefig(scheme + "_step%i.jpg"%n)
+        plt.show()
+        plt.pause(0.05)
+    
+    #FTBS - computes the numerical solution and calls the plotting function
+    def ftbs(self):
+        scheme='FTBS'
+        
+        for n in range(self.nt):
+            for j in range(1,self.nx+1):
+                self.phi[j] = self.phiOld[j] - self.c*(self.phiOld[j] - self.phiOld[j-1])
+            self.phi[0]=self.phi[-1] 
+            self.phiOld = self.phi.copy()
             
+            #selecting and plotting 10 timesteps
+            y=self.nt/10
+            if n%y==0:
+                self.plot_timestep(n,scheme)
+                
+    #FTCS - computes the numerical solution and calls the plotting function
+    def ftcs(self):
+        scheme='FTCS'
+        
+        for n in range(self.nt):
+            for j in range(1,self.nx):
+                self.phi[j] = self.phiOld[j] - self.c*(self.phiOld[j+1] - self.phiOld[j-1])/2
+            self.phi[0]=self.phiOld[0]-self.c*(self.phiOld[1]-self.phiOld[-2])/2    
+            self.phi[-1]=self.phi[0] 
+            self.phiOld = self.phi.copy()
+            
+            #selecting and plotting 10 timesteps
+            y=self.nt/10
+            if n%y==0:
+                self.plot_timestep(n,scheme)
+    
+    #CTCS - computes the numerical solution and calls the plotting function
+    def ctcs(self):
+        scheme='FTCS'
+        self.phiOld=self.analytic(self.dt)
+        
+        for n in range(2,self.nt):
+            for j in range(1,self.nx):
+                self.phi[j] = self.phiOlder[j] - self.c*(self.phiOld[j+1] - self.phiOld[j-1])
+            self.phi[0]=self.phiOlder[0]-self.c*(self.phiOld[1]-self.phiOld[-2])   
+            self.phi[-1]=self.phi[0] 
+            self.phiOlder = self.phiOld.copy()
+            self.phiOld = self.phi.copy()
+            
+            #selecting and plotting 10 timesteps
+            y=self.nt/10
+            if n%y==0:
+                self.plot_timestep(n,scheme) 
         
