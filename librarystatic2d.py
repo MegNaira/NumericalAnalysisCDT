bbdf964a70d777f6a020d64cfcee0ce4634a97cc
Arianna Ferrotti <ariannaferrotti@gmail.com>
2024-12-11 11:57:18 +0000

    added functions to find triangles where points reside, and evaluate functions on them. Also added the (very pen-and-paper like, function xi_from_x

--- librarystatic2d.py
+++ librarystatic2d.py
@@ -15,11 +15,12 @@ def shape(xi):
 
     Parameters
     ----------
-    xi : array containing the coordinates of a point in the reference triangle.
+    xi: array of floats 2x1
+        local coordinates of a point in the reference triangle
 
     Returns
     -------
-    Array of floats
+    Array of floats 3x1
         Ordered shape function on points of reference triangle.
 
     """
@@ -39,19 +40,22 @@ def dNdxi():
 
     Returns
     -------
-    Array of floats
+    Array of floats 3x2
         Ordered derivative of shape functions wrt xi[0],  xi[1].
 
     """
     return np.array([[-1.,-1.],[1.,0.],[0.,1.]])
 
-def x_from_xi(xi, xe):
+def x_from_xi(xi, xn):
     """
     Returns the global coordinates of a point xi on the reference triangle, given the global coordinates of the element nodes
 
     Parameters
     ----------
-    xe: array of floats
+    xi: array of floats 2x1
+        local coordinates of a point in the reference triangle
+    
+    xn: array of floats 3x2
         global coordinates of element nodes
 
     Returns
@@ -62,9 +66,33 @@ def x_from_xi(xi, xe):
     """
 
     N=shape(xi)
-    x=N[0]*xe[0,0]+N[1]*xe[1,0]+N[2]*xe[2,0]
-    y=N[0]*xe[0,1]+N[1]*xe[1,1]+N[2]*xe[2,1]
+    x=N[0]*xn[0,0]+N[1]*xn[1,0]+N[2]*xn[2,0]
+    y=N[0]*xn[0,1]+N[1]*xn[1,1]+N[2]*xn[2,1]
     return np.array([x,y])
+
+def xi_from_x(x, xn):
+    """
+    Returns the local coordinates of a point x on the element triangle, given the global coordinates of the element nodes
+
+    Parameters
+    ----------
+    xn: array of floats 3x2
+        global coordinates of element nodes
+    
+
+    Returns
+    -------
+    Array of floats
+        Global coordinates of a point in the reference triangle mapped back to the element triangle
+
+    """
+    A=(x[0]-xn[0,0])/(xn[1,0]-xn[0,0])
+    B=(xn[2,0]-xn[0,0])/(xn[1,0]-xn[0,0])
+    C=(x[1]-xn[0,1])/(xn[2,1]-xn[0,1])
+    D=(xn[1,1]-xn[0,1])/(xn[2,1]-xn[0,1])
+    xi1=(A-B*C)/(1-B*D)
+    xi2=C-D*((A-B*C)/(1-B*D))
+    return np.array([xi1,xi2])
 
 def jacobian(xe):
     """
@@ -347,3 +375,54 @@ def local_force(S,xe):
         f[i]=quadrature_over_ref(integrand)
         
     return f
+
+
+def closest_node(xe):
+    Rx=np.array([473993, 171625])
+    distance=np.zeros(len(xe[:,0]))
+    for i in range(len(distance)):
+        distance[i]=np.sqrt((xe[i,0]-Rx[0])**2+(xe[i,1]-Rx[1])**2)
+                                                                                                                                                     
+    return np.where(distance == distance.min())
+
+def candidates(xe,IEN):
+    gnn=closest_node(xe)
+    numbers=[]
+    for e in range(len(IEN[:,0])):
+        if (gnn==IEN[e,0]) or (gnn==IEN[e,1]) or (gnn==IEN[e,2]):
+            numbers.append(IEN[e,:])
+            
+    return np.array(numbers)
+    
+    
+def triangle_finder(xe,IEN,x):
+    cands=candidates(xe,IEN)
+    
+    for e in range(len(cands[:,0])):
+        Rxi=xi_from_x(x, xe[cands[e,:],:])
+        if (Rxi[0]>=0) and (Rxi[1]>=0) and ((Rxi[0]+Rxi[1])<=1):
+            #print("Triangle found! It's candidate number",e+1)
+            break
+        
+    return xe[cands[e,:],:], cands[e,:], Rxi
+  
+def psi_r(xe,IEN,psi):
+    Rx=np.array([473993, 171625])
+    tri_coords, tri_num, Rxi = triangle_finder(xe,IEN,Rx)
+    psi_tri=psi[tri_num]
+    
+    N=shape(Rxi)
+    psi_R=N[0]*psi_tri[0]+N[1]*psi_tri[1]+N[2]*psi_tri[2]
+    
+    return psi_R
+
+def psi_s(xe,IEN,psi):
+    Sx=np.array([442365, 115483])
+    tri_coords, tri_num, Sxi = triangle_finder(xe,IEN,Sx)
+    psi_tri=psi[tri_num]
+    
+    N=shape(Sxi)
+    psi_S=N[0]*psi_tri[0]+N[1]*psi_tri[1]+N[2]*psi_tri[2]
+    
+    return psi_S
+    
